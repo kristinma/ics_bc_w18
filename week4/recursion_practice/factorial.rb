b7ad8336ab42509946dba21118e0ac40c07e0ba9
kristinma <mblp00@gmail.com>
2019-01-16 21:40:43 -0800

    complete week 4

--- week4/recursion_practice/factorial.rb
+++ week4/recursion_practice/factorial.rb
@@ -2,10 +2,13 @@
 # See if you can figure out how to write the method!
 
 def factorial(n)
-
-  # YOUR CODE HERE #
-
+  if n == 0
+    return 1
+  else
+    n * factorial(n - 1)
+  end
 end
+  # YOUR CODE HERE
 
-puts(factorial(3)) # expected: 6
-puts(factorial(10)) # expected: 3628800
+# puts(factorial(3)) # expected: 6
+# puts(factorial(10)) # expected: 3628800
